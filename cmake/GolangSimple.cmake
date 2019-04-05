c77dd94fcf686b33bb4e11d9871ae3876ac2718d
Federico Gimenez <federico.gimenez@gmail.com>
2019-04-05 10:36:56 +0200

    aligned go and rust build and install processes

--- cmake/GolangSimple.cmake
+++ cmake/GolangSimple.cmake
@@ -10,7 +10,7 @@ function(ADD_GO_INSTALLABLE_PROGRAM NAME MAIN_SRC)
   get_filename_component(MAIN_SRC_ABS ${MAIN_SRC} ABSOLUTE)
   add_custom_target(${NAME})
   add_custom_command(TARGET ${NAME}
-                    COMMAND env GOPATH=${GOPATH} go build 
+                    COMMAND env GOPATH=${GOPATH} go build
                     -o "${CMAKE_CURRENT_BINARY_DIR}/${NAME}"
                     ${CMAKE_GO_FLAGS} ${MAIN_SRC}
                     WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
@@ -18,7 +18,6 @@ function(ADD_GO_INSTALLABLE_PROGRAM NAME MAIN_SRC)
   foreach(DEP ${ARGN})
     add_dependencies(${NAME} ${DEP})
   endforeach()
-  
+
   add_custom_target(${NAME}_all ALL DEPENDS ${NAME})
-  install(PROGRAMS ${CMAKE_CURRENT_BINARY_DIR}/${NAME} DESTINATION bin)
 endfunction(ADD_GO_INSTALLABLE_PROGRAM)
