98a05eef15f1c27653ffc204d15088c940880231
Skalman <skalman@riseup.net>
2019-06-10 15:30:00 -0400

    Rust trie test is working. Go implmeentation moved to go.mod, fails for not having access to go trie functions.

---
+++ scripts/rust-tester.el
@@ -0,0 +1,4 @@
+;;scale-codec
+(gdb "gdb -i=mi --cd ../build/bin/usr/local/bin/  --args ./rust_tester scale-codec encode --input 1")
+;;trie-root
+(gdb "gdb -i=mi --cd ../build/bin/usr/local/bin/  --args ./rust_tester state-trie state-root --state-file \"../../../../../test/fixtures/random_state_fixture.yaml\"")
