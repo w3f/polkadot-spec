e031220e607c447d8df4ead69fa5da50730c0128
Skalman <skalman@riseup.net>
2019-06-12 21:29:45 -0400

    state root test runs by running ./run_tests.sh but it fails at inequality of state root tries.

--- scripts/go_tester.el
+++ scripts/go_tester.el
@@ -2,4 +2,7 @@
 (gdb "gdb -i=mi --cd ../build/bin/usr/local/bin/  --args ./go_tester scale-codec encode --input 1")
 
 ;;trie-root
-(gdb "gdb -i=mi --cd ../build/bin/usr/local/bin/  --args ./go_tester state-trie state-root --state-file \"../../../../../test/fixtures/random_state_fixture.yaml\"")
+(gdb "gdb -i=mi --cd ../build/bin/usr/local/bin/  --args ./go_tester state-trie trie-root --state-file \"../../../../../test/fixtures/random_state_trie_80.yaml\"")
+
+(gdb "gdb -i=mi --cd ../build/bin/usr/local/bin/  --args ./go_tester state-trie trie-root --state-file \"../../../../../test/fixtures/1c1_trie.yaml\"")
+
