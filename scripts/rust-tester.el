aa71c3da2316fe5c12a9be1d9d21e72d7afa8095
noot <elizabethjbinks@gmail.com>
2019-06-28 08:43:07 -0400

    update testers to print hash

--- scripts/rust-tester.el
+++ scripts/rust-tester.el
@@ -2,3 +2,4 @@
 (gdb "gdb -i=mi --cd ../build/bin/usr/local/bin/  --args ./rust_tester scale-codec encode --input 1")
 ;;trie-root
 (gdb "gdb -i=mi --cd ../build/bin/usr/local/bin/  --args ./rust_tester state-trie state-root --state-file \"../../../../../test/fixtures/random_state_fixture.yaml\"")
+(gdb "gdb -i=mi --cd ../build/bin/usr/local/bin/  --args ./rust_tester state-trie state-root --state-file \"../../../../../test/fixtures/pk_branch_test.yaml\"")
