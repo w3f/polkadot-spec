2294e662016c6e079c3a66794c94de9b08e72cd2
noot <elizabethjbinks@gmail.com>
2019-06-28 08:43:07 -0400

    update testers to print hash

--- scripts/go_tester.el
+++ scripts/go_tester.el
@@ -6,3 +6,6 @@
 
 (gdb "gdb -i=mi --cd ../build/bin/usr/local/bin/  --args ./go_tester state-trie trie-root --state-file \"../../../../../test/fixtures/1c1_trie.yaml\"")
 
+(gdb "gdb -i=mi --cd ../build/bin/usr/local/bin/  --args ./go_tester state-trie trie-root --state-file \"../../../../../test/fixtures/pk_branch_test.yaml\"")
+
+
