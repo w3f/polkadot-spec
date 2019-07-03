a0468ef1d0fe24450e9fb2ab41c4220dc4335380
Skalman <skalman@riseup.net>
2019-07-03 14:50:35 -0400

    insert-and-delete test implemented for rust but for keccak only

--- scripts/rust-tester.el
+++ scripts/rust-tester.el
@@ -3,4 +3,11 @@
 ;;trie-root
 (gdb "gdb -i=mi --cd ../build/bin/usr/local/bin/  --args ./rust_tester state-trie state-root --state-file \"../../../../../test/fixtures/random_state_fixture.yaml\"")
 (gdb "gdb -i=mi --cd ../build/bin/usr/local/bin/  --args ./rust_tester state-trie trie-root --state-file \"../../../../../test/fixtures/pk_branch_test.yaml\"")
+
+(gdb "gdb -i=mi --cd ../build/bin/usr/local/bin/  --args ./rust_tester state-trie trie-root --state-file \"/home/klaymen/doc/code/polkadot-re-tests/test/fixtures/hex_limit_trie.yaml\"")
+
 (gdb "gdb -i=mi --cd ../build/bin/usr/local/bin/  --args ./rust_tester state-trie trie-root --keys-in-hex --state-file \"/home/klaymen/doc/code/polkadot-re-tests/test/fixtures/hex_limit_trie.yaml\"")
+
+;; insert-and-delete
+(gdb "gdb -i=mi --cd ../build/bin/usr/local/bin/  --args ./rust_tester state-trie insert-and-delete --keys-in-hex --state-file \"/home/klaymen/doc/code/polkadot-re-tests/test/fixtures/hex_limit_trie.yaml\"")
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
