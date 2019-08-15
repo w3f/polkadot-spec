909feb7b77da54c417712748a75f85e2e76fb48c
Harrm <abrehchs@gmail.com>
2019-08-15 20:58:41 +0300

    Update kagome dependecy

--- build_test.sh
+++ build_test.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
+git submodule update --init --recursive
+
 SCRIPT_DIR=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 cd ${SCRIPT_DIR}
 
+mkdir -p build
+
 # build kagome
 cd implementations/cpp/kagome || exit
-mkdir -p build
-cmake -DCMAKE_INSTALL_PREFIX=build/install -B build
-cmake --build build -- install || exit
+KAGOME_BUILD_DIR="${SCRIPT_DIR}/build/kagome"
+mkdir -p ${KAGOME_BUILD_DIR}
+cmake -DCMAKE_INSTALL_PREFIX=${KAGOME_BUILD_DIR}/install -B ${KAGOME_BUILD_DIR}
+cmake --build ${KAGOME_BUILD_DIR} -- install || exit
 
-cd ${SCRIPT_DIR}
-
-mkdir -p build
-cd build
+cd ${SCRIPT_DIR}/build
 
 cmake ..
 make
