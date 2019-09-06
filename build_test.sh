c5f35ef50777923aee8b4ebbf41ccb5c2b6e909c
Harrm <abrehchs@gmail.com>
2019-09-07 01:08:55 +0300

    Move kagome build script to tester's CMakeLists.txt

--- build_test.sh
+++ build_test.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
 SCRIPT_DIR=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
+
 cd ${SCRIPT_DIR}
 
 mkdir -p build
-
-# build kagome
-cd implementations/cpp/kagome || exit
-KAGOME_BUILD_DIR="${SCRIPT_DIR}/build/kagome"
-mkdir -p ${KAGOME_BUILD_DIR}
-cmake -DCMAKE_INSTALL_PREFIX=${KAGOME_BUILD_DIR}/install -B ${KAGOME_BUILD_DIR}
-cmake --build ${KAGOME_BUILD_DIR} -- install || exit
 
 cd ${SCRIPT_DIR}/build
 
