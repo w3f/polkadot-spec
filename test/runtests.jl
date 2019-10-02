e802b38fe5386494d5d7cf692f4dec272e2530a7
lamafab <42901763+lamafab@users.noreply.github.com>
2019-10-02 09:39:35 +0200

    updated test description

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,7 @@ using Test
 @time begin
   @time @testset "Scale Codec Tests" begin include("scale_codec_tests.jl") end
   @time @testset "State Trie Tests" begin include("state_trie_tests.jl") end
-  @time @testset "Runtime Environment Tests" begin include("pdre_api_tests.jl") end
+  @time @testset "RE API Tests" begin include("pdre_api_tests.jl") end
 end
 
 exit()
