eda466cf4d63a0c3dd2e342bad9df247fbf12879
lamafab <42901763+lamafab@users.noreply.github.com>
2019-10-02 15:39:00 +0200

    pass input to storage functions

--- test/testers/rust-tester/src/pdre_api/crypto.rs
+++ test/testers/rust-tester/src/pdre_api/crypto.rs
@@ -1,84 +1,92 @@
+use super::ParsedInput;
 use super::utils::CryptoApi;
 
 use substrate_primitives::hashing::{twox_128, twox_256, twox_64};
 
-pub fn test_blake2_128(input: &str) {
+pub fn test_blake2_128(input: ParsedInput) {
     let mut api = CryptoApi::new();
+    let data = input.get(0);
 
     let mut output = [0; 16];
 
-    api.rtm_ext_blake2_128(input.as_bytes(), &mut output);
+    api.rtm_ext_blake2_128(data.as_bytes(), &mut output);
     assert_eq!(
         hex::decode("b9f759e8bba7215c55babf022740e70e").unwrap(),
         output
     );
+
+    println!("Output: {}", hex::encode(output));
 }
 
-pub fn test_blake2_256(input: &str) {
+pub fn test_blake2_256(input: ParsedInput) {
     let mut api = CryptoApi::new();
+    let data = input.get(0);
 
     let mut output = [0; 32];
 
-    api.rtm_ext_blake2_256(input.as_bytes(), &mut output);
+    api.rtm_ext_blake2_256(data.as_bytes(), &mut output);
     assert_eq!(
         hex::decode("205b104b5db516e0a7b61b0243cfc0a7dee470d831e1707ebb99ae6bbd1ca70c").unwrap(),
         output
     );
+
+    println!("Output: {}", hex::encode(output));
 }
 
-pub fn test_twox_64(input: &str) {
+pub fn test_twox_64(input: ParsedInput) {
     let mut api = CryptoApi::new();
+    let data = input.get(0);
 
     let mut output = [0; 8];
 
-    api.rtm_ext_twox_64(input.as_bytes(), &mut output);
-    assert_eq!(twox_64(input.as_bytes()), output);
+    api.rtm_ext_twox_64(data.as_bytes(), &mut output);
+    assert_eq!(twox_64(data.as_bytes()), output);
 
-    println!("> testing TwoX 64-bit hash");
-    println!("  - Input : {}", input);
-    println!("  - Output : {}", hex::encode(output));
+    println!("Output: {}", hex::encode(output));
 }
 
-pub fn test_twox_128(input: &str) {
+pub fn test_twox_128(input: ParsedInput) {
     let mut api = CryptoApi::new();
+    let data = input.get(0);
 
     let mut output = [0; 16];
 
-    api.rtm_ext_twox_128(input.as_bytes(), &mut output);
-    assert_eq!(twox_128(input.as_bytes()), output);
+    api.rtm_ext_twox_128(data.as_bytes(), &mut output);
+    assert_eq!(twox_128(data.as_bytes()), output);
 
-    println!("> testing TwoX 128-bit hash");
-    println!("  - Input : {}", input);
-    println!("  - Output : {}", hex::encode(output));
+    println!("Output: {}", hex::encode(output));
 }
 
-pub fn test_twox_256(input: &str) {
+pub fn test_twox_256(input: ParsedInput) {
     let mut api = CryptoApi::new();
+    let data = input.get(0);
 
     let mut output = [0; 32];
 
-    api.rtm_ext_twox_256(input.as_bytes(), &mut output);
-    assert_eq!(twox_256(input.as_bytes()), output);
+    api.rtm_ext_twox_256(data.as_bytes(), &mut output);
+    assert_eq!(twox_256(data.as_bytes()), output);
 
-    println!("> testing TwoX 256-bit hash");
-    println!("  - Input : {}", input);
-    println!("  - Output : {}", hex::encode(output));
+    println!("Output: {}", hex::encode(output));
 }
 
-pub fn test_keccak_256(input: &str) {
+pub fn test_keccak_256(input: ParsedInput) {
     let mut api = CryptoApi::new();
+    let data = input.get(0);
 
     let mut output = [0; 32];
 
-    api.rtm_ext_keccak_256(input.as_bytes(), &mut output);
+    api.rtm_ext_keccak_256(data.as_bytes(), &mut output);
     assert_eq!(
         hex::decode("de58a0bbe5d87cf47773472428863d8d7e52c6f9251288660bbbef7afa2a6286").unwrap(),
         output
     );
+
+    println!("Output: {}", hex::encode(output));
 }
 
-pub fn test_ed25519(input: &str) { // message
+pub fn test_ed25519(input: ParsedInput) {
     let mut api = CryptoApi::new();
+    let data = input.get(0);
 
     // Generate key pair
     let keystore = String::from("dumy");
@@ -92,13 +100,13 @@ pub fn test_ed25519(input: &str) { // message
     let res = api.rtm_ext_ed25519_sign(
         keystore.as_bytes(),
         &pubkey1,
-        input.as_bytes(),
+        data.as_bytes(),
         &mut signature,
     );
     assert_eq!(res, 0);
 
     // Verify message
-    let verify = api.rtm_ext_ed25519_verify(input.as_bytes(), &signature, &pubkey1);
+    let verify = api.rtm_ext_ed25519_verify(data.as_bytes(), &signature, &pubkey1);
     assert_eq!(verify, 0);
 
     // Generate new key pair for listing
@@ -110,22 +118,21 @@ pub fn test_ed25519(input: &str) { // message
     let all_pubkeys = api.rtm_ext_ed25519_public_keys(keystore.as_bytes(), &mut result_len);
     //assert_eq!(result_len, 65); // Why 65 and not 64?
 
-    println!("> testing ed25519 keys");
-    println!("  - public key 1: {}", hex::encode(pubkey1));
-    println!("  - input/message: {}", input);
-    println!("  - signature: {}", hex::encode(&signature[..]));
-    print!("  - signature valid?: ");
+    println!("Public key 1: {}", hex::encode(pubkey1));
+    println!("Input/message: {}", data);
+    println!("Signature: {}", hex::encode(&signature[..]));
     if verify == 0 {
         println!("GOOD SIGNATURE");
     } else {
         println!("BAD SIGNATURE");
     }
-    println!("  - public key 2: {}", hex::encode(pubkey2));
-    println!("  - all public keys : {}", hex::encode(&all_pubkeys[1..])); // TODO; should be [..]
+    println!("Public key 2: {}", hex::encode(pubkey2));
+    println!("All public keys : {}", hex::encode(&all_pubkeys[1..])); // TODO; should be [..]
 }
 
-pub fn test_sr25519(input: &str) {
+pub fn test_sr25519(input: ParsedInput) {
     let mut api = CryptoApi::new();
+    let data = input.get(0);
 
     // Generate key pair
     let keystore = String::from("dumy");
@@ -139,12 +146,12 @@ pub fn test_sr25519(input: &str) {
     let res = api.rtm_ext_sr25519_sign(
         keystore.as_bytes(),
         &pubkey1,
-        input.as_bytes(),
+        data.as_bytes(),
         &mut signature,
     );
     assert_eq!(res, 0);
 
-    let verify = api.rtm_ext_sr25519_verify(input.as_bytes(), &signature, &pubkey1);
+    let verify = api.rtm_ext_sr25519_verify(data.as_bytes(), &signature, &pubkey1);
     assert_eq!(verify, 0);
 
     // Generate new key pair for listing
@@ -156,16 +163,14 @@ pub fn test_sr25519(input: &str) {
     let all_pubkeys = api.rtm_ext_sr25519_public_keys(keystore.as_bytes(), &mut result_len);
     assert_eq!(result_len, 65);
 
-    println!("> testing sr25519 keys");
-    println!("  - public key 1: {}", hex::encode(pubkey1));
-    println!("  - input/message: {}", input);
-    println!("  - signature: {}", hex::encode(&signature[..]));
-    print!("  - signature valid?: ");
+    println!("Public key 1: {}", hex::encode(pubkey1));
+    println!("Input/message: {}", data);
+    println!("Signature: {}", hex::encode(&signature[..]));
     if verify == 0 {
         println!("GOOD SIGNATURE");
     } else {
         println!("BAD SIGNATURE");
     }
-    println!("  - public key 2: {}", hex::encode(pubkey2));
-    println!("  - all public keys : {}", hex::encode(&all_pubkeys[1..])); // TODO; should be [..]
+    println!("Public key 2: {}", hex::encode(pubkey2));
+    println!("All public keys : {}", hex::encode(&all_pubkeys[1..])); // TODO; should be [..]
 }
--- test/testers/rust-tester/src/pdre_api/mod.rs
+++ test/testers/rust-tester/src/pdre_api/mod.rs
@@ -5,14 +5,14 @@ mod utils;
 
 use clap::{ArgMatches, Values};
 
-struct ParsedInput<'a>(Vec<&'a str>);
+pub struct ParsedInput<'a>(Vec<&'a str>);
 
 impl<'a> ParsedInput<'a> {
-    fn get(&self, index: usize) -> &str {
+    pub fn get(&self, index: usize) -> &str {
         if let Some(ret) = self.0.get(index) {
             ret
         } else {
-            panic!("wrong input data provided for the test function");
+            panic!("failed to get index, wrong input data provided for the test function");
         }
     }
 }
@@ -25,24 +25,33 @@ impl<'a> From<Values<'a>> for ParsedInput<'a> {
 
 pub fn process_pdre_api_tests(subcmd_matches: &ArgMatches) {
     if let Some(func) = subcmd_matches.value_of("function") {
-        if let Some(input) = subcmd_matches.values_of("input") {
+        let input: ParsedInput = subcmd_matches.values_of("input").unwrap().into();
 
-        } else {
-
-        }
-
-/*
         match func {
-            "test_blake2_128" => crypto::test_blake2_128(),
-            "test_blake2_256" => crypto::test_blake2_256(),
-            "test_ed25519" => crypto::test_ed25519(),
-            "test_keccak_256" => crypto::test_keccak_256(),
-            "test_sr25519" => crypto::test_sr25519(),
-            "test_twox_64" => crypto::test_twox_64(),
-            "test_twox_128" => crypto::test_twox_128(),
-            "test_twox_256" => crypto::test_twox_256(),
+            // test crypto functions
+            "test_blake2_128" => crypto::test_blake2_128(input),
+            "test_blake2_256" => crypto::test_blake2_256(input),
+            "test_ed25519" => crypto::test_ed25519(input),
+            "test_keccak_256" => crypto::test_keccak_256(input),
+            "test_sr25519" => crypto::test_sr25519(input),
+            "test_twox_64" => crypto::test_twox_64(input),
+            "test_twox_128" => crypto::test_twox_128(input),
+            "test_twox_256" => crypto::test_twox_256(input),
+            // test storage functions
+            "test_allocate_storage" => storage::test_allocate_storage(),
+            "test_clear_child_prefix" => storage::test_clear_child_prefix(),
+            "test_clear_child_storage" => storage::test_clear_child_storage(input),
+            "test_clear_prefix" => storage::test_clear_prefix(),
+            "test_clear_storage" => storage::test_clear_storage(input),
+            "test_exists_child_storage" => storage::test_exists_child_storage(input),
+            "test_exists_storage" => storage::test_exists_storage(input),
+            "test_kill_child_storage" => storage::test_kill_child_storage(input),
+            "test_set_get_child_storage" => storage::test_set_get_child_storage(input),
+            "test_set_get_local_storage" => storage::test_set_get_local_storage(),
+            "test_set_get_storage" => storage::test_set_get_storage(input),
+            "test_storage_root" => storage::test_storage_root(),
+            _ => panic!("specified functio not available"),
         }
-        */
     }
 
     /*
