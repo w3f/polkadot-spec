977ff2b91b0ae82c7927f9155e57c0bb52701112
lamafab <42901763+lamafab@users.noreply.github.com>
2019-10-03 15:33:21 +0200

    expanded test coverage to storage

--- pdre_api_tests.jl
+++ pdre_api_tests.jl
@@ -6,22 +6,69 @@ using Test
     manifest_dir = script_dir * "test/testers/rust-tester/Cargo.toml"
     cd(root_dir)
     for (cli, sub_cmd, func_arg, input_arg) in PdreApiTestFixtures.cli_testers
-        # Test crypto functions
+        # ## Test crypto functions
         for func in PdreApiTestFixtures.fn_crypto
             for value in PdreApiTestData.value_data
                 # create first part of the command
                 cmdparams = [cli, sub_cmd, func_arg, func, input_arg]
                 cmd = join(cmdparams, " ")
 
-                inputparams = [" \"", value, "\""]
-                input = join(inputparams)
-
                 # append input
-                cmd = string(cmd, input)
-                @test read(`sh -c $cmd`, String) != 1
+                cmd = string(cmd, " \"", value,"\"")
+
+                # Run
+                println(">> Running:", cmd)
+                output = read(`sh -c $cmd`, String)
+                if output != ""
+                    println(output)
+                end
+                @test true
             end
         end
-        # run(`cargo run pdre-api`)
+
+        # ## Test storage functions (key/value inputs and outputs)
+        for func in PdreApiTestFixtures.fn_storage_kv
+            for (key, value) in PdreApiTestData.key_value_data
+                # create first part of the command
+                cmdparams = [cli, sub_cmd, func_arg, func, input_arg]
+                cmd = join(cmdparams, " ")
+
+                input = join([key, value], ",")
+
+                # append input
+                cmd = string(cmd, " \"", input,"\"")
+
+                # Run
+                println(">> Running:", cmd)
+                output = read(`sh -c $cmd`, String)
+                if output != ""
+                    println(output)
+                end
+                @test true
+            end
+        end
+
+        # ## Test storage functions (prefix values)
+        for func in PdreApiTestFixtures.fn_storage_prefix
+            for (prefix, key1, value1, key2, value2) in PdreApiTestData.prefix_key_value_data
+                # create first part of the command
+                cmdparams = [cli, sub_cmd, func_arg, func, input_arg]
+                cmd = join(cmdparams, " ")
+
+                input = join([prefix, key1, value1, key2, value2], ",")
+
+                # append input
+                cmd = string(cmd, " \"", input,"\"")
+
+                # Run
+                println(">> Running:", cmd)
+                output = read(`sh -c $cmd`, String)
+                if output != ""
+                    println(output)
+                end
+                @test true
+            end
+        end
     end
     cd(root_dir)
 end
