47ed7b4dca7aedb0f203af2ab2ef5f0ac8d58787
lamafab <42901763+lamafab@users.noreply.github.com>
2019-10-08 16:18:43 +0200

    support conditional verbose output

--- runtests.jl
+++ runtests.jl
@@ -1,5 +1,12 @@
 using Test
 
+print_verbose = false
+if length(ARGS) >= 1
+    if ARGS[1] == "verbose"
+        print_verbose = true
+    end
+end
+
 @time begin
   @time @testset "Scale Codec Tests" begin include("scale_codec_tests.jl") end
   @time @testset "State Trie Tests" begin include("state_trie_tests.jl") end
