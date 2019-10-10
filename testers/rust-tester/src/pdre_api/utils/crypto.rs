0e68a82fe964ee232624a8bc4ed0904fabc25173
lamafab <42901763+lamafab@users.noreply.github.com>
2019-10-10 15:46:56 +0200

    gen params for ed25519_public_keys

--- testers/rust-tester/src/pdre_api/utils/crypto.rs
+++ testers/rust-tester/src/pdre_api/utils/crypto.rs
@@ -129,72 +129,26 @@ impl CryptoApi {
         );
 
         res.unwrap()
-
-        /*
-        WasmExecutor::new()
-            .call_with_custom_signature(
-                &mut self.ext,
-                1,
-                &self.blob,
-                "test_ext_ed25519_verify",
-                |alloc| {
-                    let msg_data_offset = alloc(msg_data)?;
-                    let sig_data_offset = alloc(sig_data)?;
-                    let pubkey_data_offset = alloc(pubkey_data)?;
-                    Ok(vec![
-                        I32(msg_data_offset as i32),
-                        I32(msg_data.len() as i32),
-                        I32(sig_data_offset as i32),
-                        I32(pubkey_data_offset as i32),
-                    ])
-                },
-                |res, _| {
-                    if let Some(I32(r)) = res {
-                        Ok(Some(r as u32))
-                    } else {
-                        Ok(None)
-                    }
-                },
-            )
-            .unwrap()
-            */
     }
     pub fn rtm_ext_ed25519_public_keys(&mut self, id_data: &[u8], result_len: &mut u32) -> Vec<u8> {
-        let ptr_holder: Rc<RefCell<u32>> = Rc::new(RefCell::new(0));
-        WasmExecutor::new()
-            .call_with_custom_signature(
-                &mut self.ext,
-                1,
-                &self.blob,
-                "test_ext_ed25519_public_keys",
-                |alloc| {
-                    let id_data_offset = alloc(id_data)?;
-                    let result_len_offset = alloc(&[0, 4])?;
-                    *ptr_holder.borrow_mut() = result_len_offset as u32;
-                    Ok(vec![
-                        I32(id_data_offset as i32),
-                        I32(result_len_offset as i32),
-                    ])
-                },
-                |res, memory| {
-                    use std::convert::TryInto;
-                    if let Some(I32(r)) = res {
-                        *result_len = u32::from_le_bytes(
-                            memory.get(*ptr_holder.borrow(), 4).unwrap().as_slice()[0..4]
-                                .try_into()
-                                .unwrap(),
-                        );
+        let mut wasm = self.prep_wasm("test_ext_ed25519_public_keys");
+        let result_len_scoped = wrap(0);
+        let ptr = wrap(0);
 
-                        memory
-                            .get(r as u32, *result_len as usize)
-                            .map_err(|_| Error::Runtime)
-                            .map(Some)
-                    } else {
-                        Ok(None)
-                    }
-                },
-            )
-            .unwrap()
+        let res = wasm.call(
+            CallWasm::gen_params(
+                &[
+                    id_data,
+                    &result_len.to_le_bytes(),
+                ],
+                &[],
+                Some(ptr.clone())
+            ),
+            CallWasm::return_buffer(result_len_scoped.clone(), ptr)
+        );
+
+        *result_len = *result_len_scoped.borrow();
+        res.unwrap()
     }
     pub fn rtm_ext_sr25519_generate(&mut self, id_data: &[u8], seed: &[u8], output: &mut [u8]) {
         let ptr_holder: Rc<RefCell<u32>> = Rc::new(RefCell::new(0));
