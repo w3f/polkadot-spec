3cd6e3b464b52be55bc3c24b33b79236dc24ebb0
lamafab <42901763+lamafab@users.noreply.github.com>
2019-10-11 09:58:32 +0200

    cleanup network utils and tests

--- test/testers/rust-tester/src/pdre_api/utils/network.rs
+++ test/testers/rust-tester/src/pdre_api/utils/network.rs
@@ -3,18 +3,13 @@
 //!
 //! Not relevant for other implementators. Look at the `tests/` directory for the acutal tests.
 
-use super::{copy_u32, copy_slice, get_wasm_blob, le, wrap, CallWasm};
+use super::{copy_u32, get_wasm_blob, le, wrap, CallWasm};
 
 use parking_lot::RwLock;
-use substrate_executor::error::Error;
-use substrate_executor::WasmExecutor;
 use substrate_offchain::testing::{PendingRequest, State, TestOffchainExt};
 use substrate_primitives::Blake2Hasher;
 use substrate_state_machine::TestExternalities as CoreTestExternalities;
-use wasmi::RuntimeValue::{I32, I64};
 
-use std::cell::RefCell;
-use std::rc::Rc;
 use std::sync::Arc;
 
 type TestExternalities<H> = CoreTestExternalities<H, u64>;
@@ -66,78 +61,26 @@ impl NetworkApi {
         copy_u32(written_out_scoped, written_out);
         res.unwrap()
     }
+    #[allow(unused)] // temporarly
     pub fn rtm_ext_http_request_add_header(
         &mut self,
         request_id: u32,
         name: &[u8],
         value: &[u8],
     ) -> u32 {
+        let _ = request_id;
+        let _ = name;
+        let _ = value;
+        0
+
         // TODO...
-        WasmExecutor::new()
-            .call_with_custom_signature(
-                &mut self.ext,
-                1,
-                &self.blob,
-                "test_ext_http_request_add_header",
-                |alloc| {
-                    let name_offset = alloc(name)?;
-                    let value_offset = alloc(value)?;
-                    Ok(vec![
-                        I32(request_id as i32),
-                        I32(name_offset as i32),
-                        I32(name.len() as i32),
-                        I32(value_offset as i32),
-                        I32(value.len() as i32),
-                    ])
-                },
-                |res, _| {
-                    if let Some(I32(r)) = res {
-                        Ok(Some(r as u32))
-                    } else {
-                        Ok(None)
-                    }
-                },
-            )
-            .unwrap()
     }
     #[allow(unused)] // temporarly
     pub fn rtm_ext_http_response_wait(&mut self, ids: &[u32], statuses: &mut [u8], deadline: u64) {
-        // TODO...
-        let ptr_holder: Rc<RefCell<u32>> = Rc::new(RefCell::new(0));
-        let ids_len = ids.len() * 4; // 4-bytes per id
-        WasmExecutor::new()
-            .call_with_custom_signature(
-                &mut self.ext,
-                1,
-                &self.blob,
-                "test_ext_http_response_wait",
-                |alloc| {
-                    // Prepare IDs
-                    let mut b_ids = vec![];
-                    for id in ids {
-                        b_ids.extend_from_slice(&id.to_le_bytes());
-                    }
-                    let ids_offset = alloc(b_ids.as_slice())?;
+        let _ = ids;
+        let _ = statuses;
+        let _ = deadline;
 
-                    let statuses_offset = alloc(vec![0; ids_len].as_slice())?;
-                    *ptr_holder.borrow_mut() = statuses_offset as u32;
-                    Ok(vec![
-                        I32(ids_offset as i32),
-                        I32(b_ids.len() as i32),
-                        I32(statuses_offset as i32),
-                        I64(deadline.to_le() as i64),
-                    ])
-                },
-                |_, memory| {
-                    statuses.copy_from_slice(
-                        memory
-                            .get(*ptr_holder.borrow(), ids_len)
-                            .map_err(|_| Error::Runtime)?
-                            .as_slice(),
-                    );
-                    Ok(Some(()))
-                },
-            )
-            .unwrap()
+        // TODO...
     }
 }
