b8e975c06a536bdb032fe1787d9a2338fd936ec6
artyom-yurin <artem_yrin@mail.ru>
2019-11-06 21:33:46 +0300

    Added ext_exists_storage test function Added declarations for another test functions
    Signed-off-by: artyom-yurin <artem_yrin@mail.ru>

---
+++ test/testers/cpp-tester/extension/crypto.cpp
@@ -0,0 +1,34 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "crypto.hpp"
+
+namespace crypto {
+
+  // Input: data
+  void processExtBlake2_128(const std::vector<std::string> &args){}
+
+  // Input: data
+  void processExtBlake2_256(const std::vector<std::string> &args){}
+
+  // Input: data
+  void processExtEd25519(const std::vector<std::string> &args){}
+
+  // Input: data
+  void processExtKeccak256(const std::vector<std::string> &args){}
+
+  // Input: data
+  void processExtSr25519(const std::vector<std::string> &args){}
+
+  // Input: data
+  void processExtTwox64(const std::vector<std::string> &args){}
+
+  // Input: data
+  void processExtTwox128(const std::vector<std::string> &args){}
+
+  // Input: data
+  void processExtTwox256(const std::vector<std::string> &args){}
+
+}
