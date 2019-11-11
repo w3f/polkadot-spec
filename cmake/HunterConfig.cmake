cc8b93a3097011b913126fa1717e885442f9ad94
Harrm <abrehchs@gmail.com>
2019-11-11 17:50:07 +0300

    Fix GTest hunter config and location of kagome in tester

--- cmake/HunterConfig.cmake
+++ cmake/HunterConfig.cmake
@@ -4,6 +4,11 @@ hunter_config(
     VERSION 1.70.0-p0
 )
 
+hunter_config(
+    GTest
+    VERSION 1.8.0-hunter-p11
+    CMAKE_ARGS "CMAKE_CXX_FLAGS=-Wno-deprecated-copy"
+)
 
 hunter_config(
     libp2p
