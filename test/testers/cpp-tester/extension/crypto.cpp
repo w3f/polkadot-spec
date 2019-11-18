283ae77f466afda02bd1e219a7075834dd234711
artyom-yurin <artem_yrin@mail.ru>
2019-11-19 00:03:12 +0300

    Added ext_keccak_256 test function
    Signed-off-by: artyom-yurin <artem_yrin@mail.ru>

--- test/testers/cpp-tester/extension/crypto.cpp
+++ test/testers/cpp-tester/extension/crypto.cpp
@@ -49,7 +49,33 @@ namespace crypto {
   void processExtEd25519(const std::vector<std::string> &args){}
 
   // Input: data
-  void processExtKeccak256(const std::vector<std::string> &args){}
+  void processExtKeccak256(const std::vector<std::string> &args){
+    std::string data = args[0];
+
+    auto db = std::make_unique<kagome::storage::InMemoryStorage>();
+    std::unique_ptr<kagome::storage::trie::TrieDb> trie =
+        std::make_unique<kagome::storage::trie::PolkadotTrieDb>(std::move(db));
+    std::shared_ptr<kagome::runtime::WasmMemory> memory =
+        std::make_shared<kagome::runtime::WasmMemoryImpl>(4096);
+
+    std::unique_ptr<kagome::extensions::Extension> extension =
+        std::make_unique<kagome::extensions::ExtensionImpl>(memory,
+                                                            std::move(trie));
+
+    kagome::common::Buffer buffer;
+
+    buffer.put(data);
+    kagome::runtime::SizeType valueSize = buffer.size();
+    kagome::runtime::WasmPointer valuePtr = memory->allocate(valueSize);
+    memory->storeBuffer(valuePtr, buffer);
+    buffer.clear();
+
+    auto resultPtr = memory->allocate(32);
+    extension->ext_keccak_256(valuePtr, valueSize, resultPtr);
+    auto hash = memory->loadN(resultPtr, 32);
+
+    std::cout << kagome::common::hex_lower(hash) << "\n";
+  }
 
   // Input: data
   void processExtSr25519(const std::vector<std::string> &args){}
