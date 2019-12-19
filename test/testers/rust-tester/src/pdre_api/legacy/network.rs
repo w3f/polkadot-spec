7e8fbf2433c7eceab98d14255998ef3d26c83823
lamafab <42901763+lamafab@users.noreply.github.com>
2019-12-19 12:25:26 +0100

    adjust network for Runtime

--- test/testers/rust-tester/src/pdre_api/legacy/network.rs
+++ test/testers/rust-tester/src/pdre_api/legacy/network.rs
@@ -1,23 +1,30 @@
-use super::utils::NetworkApi;
+use super::utils::{Runtime, Decoder};
+use parity_scale_codec::Encode;
 
 pub fn test_http() {
-    let mut api = NetworkApi::new_with_offchain_context();
+    let mut rtm = Runtime::new_offchain();
 
     // Request 1
-    let req_id1 = api.rtm_ext_http_request_start("GET".as_bytes(), "example.com".as_bytes(), &[]);
+    let req_id1 = rtm
+        .call("rtm_ext_http_request_start", &("GET", "example.com", "").encode())
+        .decode_u32();
     assert_eq!(req_id1, 0);
 
     // Request 2
-    let req_id2 = api.rtm_ext_http_request_start("GET".as_bytes(), "example.com".as_bytes(), &[]);
+    let req_id2 = rtm
+        .call("rtm_ext_http_request_start", &("GET", "example.com", "").encode())
+        .decode_u32();
     assert_eq!(req_id2, 1);
 
     // TODO...
 }
 
 pub fn test_network_state() {
-    let mut api = NetworkApi::new_with_offchain_context();
+    let mut rtm = Runtime::new_offchain();
 
-    let res = api.rtm_ext_network_state();
+    let res = rtm
+        .call("rtm_ext_network_state", &[])
+        .decode_vec();
     assert_eq!(res, vec![0, 0, 0]); // [PeerId, MultiaddressIPv4, MultiaddressIPv6]
 
     println!("Output (opaque): {:?}", res);
