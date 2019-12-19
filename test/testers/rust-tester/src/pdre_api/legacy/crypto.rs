0b3fd7a3a5ccd8988eef9d9f180ee83e9ba6c057
lamafab <42901763+lamafab@users.noreply.github.com>
2019-12-19 11:22:55 +0100

    adjust test_twox_256 for Runtime

--- test/testers/rust-tester/src/pdre_api/legacy/crypto.rs
+++ test/testers/rust-tester/src/pdre_api/legacy/crypto.rs
@@ -72,11 +72,13 @@ pub fn test_twox_128(input: ParsedInput) {
 
 // Input: data
 pub fn test_twox_256(input: ParsedInput) {
-    let mut api = CryptoApi::new();
+    let mut rtm = Runtime::new();
 
     let data = input.get(0);
 
-    let output =  api.rtm_ext_twox_256(data);
+    let output = rtm
+        .call("rtm_ext_twox_256", &data.encode())
+        .decode_vec();
     assert_eq!(twox_256(data), output.as_slice());
 
     println!("{}", hex::encode(output));
