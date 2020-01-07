5d8305dc26e13fe93eb829c463fd6b79f4dd5baf
lamafab <42901763+lamafab@users.noreply.github.com>
2020-01-07 15:14:12 +0100

    adjusted TODO comments in process_pdre_api_tests

--- test/testers/rust-tester/rust_tester/src/pdre_api/mod.rs
+++ test/testers/rust-tester/rust_tester/src/pdre_api/mod.rs
@@ -23,7 +23,7 @@ pub fn process_pdre_api_tests(subcmd_matches: &ArgMatches) {
             "test_twox_64" => crypto::test_twox_64(input),
             "test_twox_128" => crypto::test_twox_128(input),
             "test_twox_256" => crypto::test_twox_256(input),
-            // TODO: Call from Julia
+            // TODO: Not fully implemented/tested
             "test_secp256k1_ecdsa_recover" => crypto::test_secp256k1_ecdsa_recover(input),
             //
             // test storage functions
@@ -35,7 +35,7 @@ pub fn process_pdre_api_tests(subcmd_matches: &ArgMatches) {
             "test_set_get_storage" => storage::test_set_get_storage(input),
             "test_set_get_storage_into" => storage::test_set_get_storage_into(input),
             "test_storage_root" => storage::test_storage_root(input),
-            // TODO: Call from Julia
+            // TODO: Not fully implemented/tested
             "test_storage_changes_root" => storage::test_storage_changes_root(input),
             "test_local_storage_compare_and_set" => storage::test_local_storage_compare_and_set(input),
             //
@@ -53,17 +53,17 @@ pub fn process_pdre_api_tests(subcmd_matches: &ArgMatches) {
             "test_network_state" => network::test_network_state(),
             //
             // miscellaneous functions
-            // TODO: Call from Julia
+            // TODO: Not fully implemented/tested
             "test_chain_id" => misc::test_chain_id(),
-            // TODO: Call from Julia
+            // TODO: Not fully implemented/tested
             "test_is_validator" => misc::test_is_validator(),
-            // TODO: Call from Julia
+            // TODO: Not fully implemented/tested
             "test_submit_transaction" => misc::test_submit_transaction(input),
-            // TODO: Call from Julia
+            // TODO: Not fully implemented/tested
             "test_timestamp" => misc::test_timestamp(),
-            // TODO: Call from Julia
+            // TODO: Not fully implemented/tested
             "test_sleep_until" => misc::test_sleep_until(input),
-            // TODO: Call from Julia
+            // TODO: Not fully implemented/tested
             "test_random_seed" => misc::test_random_seed(),
             //
             _ => panic!("specified function not available"),
