2879f7250d247389ced0c8bfedc554e6bb66bf7d
lamafab <42901763+lamafab@users.noreply.github.com>
2020-01-07 13:05:59 +0100

    adjust paths to wasm blob in util module

--- testers/rust-tester/rust_tester/src/pdre_api/utils.rs
+++ testers/rust-tester/rust_tester/src/pdre_api/utils.rs
@@ -69,7 +69,7 @@ fn get_wasm_blob() -> Vec<u8> {
     
     let mut f =
     // for `run_tests.sh` in root directory
-    File::open("build/test/testers/rust-tester/x86_64-unknown-linux-gnu/debug/wbuild/legacy-wasm-blob/legacy_pdre_tester_wasm_blob.compact.wasm")
+    File::open("build/test/testers/rust-tester/x86_64-unknown-linux-gnu/debug/wbuild/wasm-blob/wasm_blob.compact.wasm")
     // for `cargo` inside rust-tester directory
     //File::open("target/debug/wbuild/legacy-wasm-blob/legacy_pdre_tester_wasm_blob.compact.wasm")
         .expect("Failed to open wasm blob in target");
