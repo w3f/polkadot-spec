b66f70675f4f1eb794a99ab8454bec32f84d531d
lamafab <42901763+lamafab@users.noreply.github.com>
2020-01-16 16:29:41 +0100

    test for ext_storage_get

---
+++ testers/rust-tester/rust_tester/src/pdre_api/storage.rs
@@ -0,0 +1,36 @@
+use crate::pdre_api::utils::{Decoder, ParsedInput, Runtime};
+use parity_scale_codec::Encode;
+
+fn str<'a>(input: &'a [u8]) -> &'a str {
+    std::str::from_utf8(input).unwrap()
+}
+
+// Input: key, value
+pub fn ext_storage_get(input: ParsedInput) {
+    let mut rtm = Runtime::new();
+
+    let key = input.get(0);
+    let value = input.get(1);
+
+    // Get invalid key
+    let res = rtm
+        .call("rtm_ext_storage_get", &key.encode())
+        .decode_vec();
+    assert_eq!(res, [0u8; 0]);
+
+    // Set key/value
+    let _ = rtm.call("rtm_ext_storage_set", &(key, value).encode());
+
+    // Get valid key
+    let res = rtm
+        .call("rtm_ext_storage_get", &key.encode())
+        .decode_vec();
+    assert_eq!(res, value);
+
+    println!("{}", str(&res));
+}
+
+// Input: key, value
+pub fn ext_storage_set(input: ParsedInput) {
+    // TODO
+}
