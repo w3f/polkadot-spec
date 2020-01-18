5b3b60c64dd05e29f369430339b79e9dccb53591
lamafab <42901763+lamafab@users.noreply.github.com>
2020-01-18 13:23:19 +0100

    attempt at callind new api

--- test/testers/rust-tester/rust_tester/src/pdre_api/storage.rs
+++ test/testers/rust-tester/rust_tester/src/pdre_api/storage.rs
@@ -14,20 +14,25 @@ pub fn ext_storage_get(input: ParsedInput) {
 
     // Get invalid key
     let res = rtm
-        .call("rtm_ext_storage_get", &key.encode())
-        .decode_vec();
+        .call("rtm_ext_storage_get", &key.encode());
+        //.decode_vec();
     assert_eq!(res, [0u8; 0]);
 
+    /*
     // Set key/value
-    let _ = rtm.call("rtm_ext_storage_set", &(key, value).encode());
+    let mut data = vec![];
+    data.extend_from_slice(&key);
+    data.extend_from_slice(&value);
+    let _ = rtm.call("rtm_ext_storage_set", &data);
 
     // Get valid key
     let res = rtm
-        .call("rtm_ext_storage_get", &key.encode())
-        .decode_vec();
+        .call("rtm_ext_storage_get", &key.encode());
+        //.decode_vec();
     assert_eq!(res, value);
 
     println!("{}", str(&res));
+    */
 }
 
 // Input: key, value
--- test/testers/rust-tester/rust_tester/wasm-blob/src/lib.rs
+++ test/testers/rust-tester/rust_tester/wasm-blob/src/lib.rs
@@ -5,34 +5,28 @@ use std::slice;
 use sp_core::wasm_export_functions;
 
 extern "C" {
-    fn ext_storage_set(key_data: *const u8, key_len: u32, value_data: *const u8, value_len: u32);
-    fn ext_storage_get(key_data: *const u8, key_len: u32) -> u64;
+    fn ext_storage_get_version_1(data: u64) -> u64;
+    fn ext_storage_set_version_1(data: u64) -> u64;
 }
 
-#[cfg(not(feature = "std"))]
-
 wasm_export_functions! {
+    fn rtm_ext_storage_get(
+        key_data: Vec<u8>,
+    ) -> u64 {
+        unsafe {
+            ext_storage_get_version_1(
+			    (key_data.len() as u64) << 32 | key_data.as_ptr() as u64,
+            )
+        }
+    }
     fn rtm_ext_storage_set(
         key_data: Vec<u8>,
         value_data: Vec<u8>,
-    ) {
+    ) -> u64 {
         unsafe {
-            ext_storage_set(
-                key_data.as_ptr(),
-                key_data.len() as u32,
-                value_data.as_ptr(),
-                value_data.len() as u32
-            );
-        }
-    }
-    fn rtm_ext_storage_get(
-        key_data: Vec<u8>,
-    ) {
-        unsafe {
-            ext_storage_get(
-                key_data.as_ptr(),
-                key_data.len() as u32,
-            );
+            ext_storage_set_version_1(
+			    (key_data.len() as u64) << 32 | key_data.as_ptr() as u64,
+            )
         }
     }
 }
