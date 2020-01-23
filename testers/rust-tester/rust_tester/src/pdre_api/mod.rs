0389f5f8b725d105a9e4361c3e76c2efa75949f6
lamafab <42901763+lamafab@users.noreply.github.com>
2020-01-23 15:30:31 +0100

    test ext_crypto_ed25519_verify

--- testers/rust-tester/rust_tester/src/pdre_api/mod.rs
+++ testers/rust-tester/rust_tester/src/pdre_api/mod.rs
@@ -19,9 +19,10 @@ pub fn process_pdre_api_tests(subcmd_matches: &ArgMatches) {
             "ext_storage_clear_prefix" => storage::ext_storage_clear_prefix(input),
             "ext_storage_root" => storage::ext_storage_root(input),
             "ext_storage_next_key" => storage::ext_storage_next_key(input),
-            "ext_crypto_ed25519_public_keys" => crypto::ext_crypto_ed25519_public_keys_version_1(),
+            "ext_crypto_ed25519_public_keys" => crypto::ext_crypto_ed25519_public_keys_version_1(input),
             "ext_crypto_ed25519_generate" => crypto::ext_crypto_ed25519_generate_version_1(input),
             "ext_crypto_ed25519_sign" => crypto::ext_crypto_ed25519_sign_version_1(input),
+            "ext_crypto_ed25519_verify" => crypto::ext_crypto_ed25519_verify_version_1(input),
             _ => panic!("specified function not available"),
         }
     }
