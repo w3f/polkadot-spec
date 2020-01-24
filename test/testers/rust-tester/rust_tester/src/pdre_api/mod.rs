343b9d0feaade10afadf6a214d99b6406a5498be
lamafab <42901763+lamafab@users.noreply.github.com>
2020-01-24 15:55:21 +0100

    test ext_storage_child_next_key

--- test/testers/rust-tester/rust_tester/src/pdre_api/mod.rs
+++ test/testers/rust-tester/rust_tester/src/pdre_api/mod.rs
@@ -27,6 +27,7 @@ pub fn process_pdre_api_tests(subcmd_matches: &ArgMatches) {
             "ext_storage_root" => storage::ext_storage_root(input),
             "ext_storage_child_root" => child_storage::ext_storage_child_root(input),
             "ext_storage_next_key" => storage::ext_storage_next_key(input),
+            "ext_storage_child_next_key" => child_storage::ext_storage_child_next_key(input),
             "ext_crypto_ed25519_public_keys" => crypto::ext_crypto_ed25519_public_keys_version_1(input),
             "ext_crypto_ed25519_generate" => crypto::ext_crypto_ed25519_generate_version_1(input),
             "ext_crypto_ed25519_sign" => crypto::ext_crypto_ed25519_sign_version_1(input),
