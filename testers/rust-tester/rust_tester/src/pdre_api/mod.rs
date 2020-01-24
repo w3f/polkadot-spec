2970300edb350f645cea51bf6470d7763bb0b51e
lamafab <42901763+lamafab@users.noreply.github.com>
2020-01-24 15:02:32 +0100

    test ext_storage_child_storage_kill

--- testers/rust-tester/rust_tester/src/pdre_api/mod.rs
+++ testers/rust-tester/rust_tester/src/pdre_api/mod.rs
@@ -19,6 +19,7 @@ pub fn process_pdre_api_tests(subcmd_matches: &ArgMatches) {
             "ext_storage_child_read" => child_storage::ext_storage_child_read(input),
             "ext_storage_clear" => storage::ext_storage_clear(input),
             "ext_storage_child_clear" => child_storage::ext_storage_child_clear(input),
+            "ext_storage_child_storage_kill" => child_storage::ext_storage_child_storage_kill_version_1(input),
             "ext_storage_exists" => storage::ext_storage_exists(input),
             "ext_storage_clear_prefix" => storage::ext_storage_clear_prefix(input),
             "ext_storage_root" => storage::ext_storage_root(input),
