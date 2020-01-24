d7dd183a0ebbf713af4a02105b0be60358f611c8
lamafab <42901763+lamafab@users.noreply.github.com>
2020-01-24 15:29:43 +0100

    test ext_storage_child_clear_prefix

--- test/testers/rust-tester/rust_tester/src/pdre_api/mod.rs
+++ test/testers/rust-tester/rust_tester/src/pdre_api/mod.rs
@@ -23,6 +23,7 @@ pub fn process_pdre_api_tests(subcmd_matches: &ArgMatches) {
             "ext_storage_exists" => storage::ext_storage_exists(input),
             "ext_storage_child_exists" => child_storage::ext_storage_child_exists_version_1(input),
             "ext_storage_clear_prefix" => storage::ext_storage_clear_prefix(input),
+            "ext_storage_child_clear_prefix" => child_storage::ext_storage_child_clear_prefix(input),
             "ext_storage_root" => storage::ext_storage_root(input),
             "ext_storage_next_key" => storage::ext_storage_next_key(input),
             "ext_crypto_ed25519_public_keys" => crypto::ext_crypto_ed25519_public_keys_version_1(input),
