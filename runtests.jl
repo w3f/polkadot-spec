e616384e3aee81895666bbf85a2390d03163b6b8
lamafab <42901763+lamafab@users.noreply.github.com>
2020-02-12 16:31:20 +0100

    temporary changes for local execution of tests

--- runtests.jl
+++ runtests.jl
@@ -8,8 +8,8 @@ if length(ARGS) >= 1
 end
 
 @time begin
-  @time @testset "Scale Codec Tests" begin include("scale_codec_tests.jl") end
-  @time @testset "State Trie Tests" begin include("state_trie_tests.jl") end
+  # @time @testset "Scale Codec Tests" begin include("scale_codec_tests.jl") end
+  # @time @testset "State Trie Tests" begin include("state_trie_tests.jl") end
   @time @testset "PDRE API Tests" begin include("pdre_api_tests.jl") end
 end
 
