74b1b0465cee06669e305780b7ba26fa5d0614b1
lamafab <42901763+lamafab@users.noreply.github.com>
2020-02-17 12:04:15 +0100

    move julia function to separate helper file

--- pdre_api_tests_legacy.jl
+++ pdre_api_tests_legacy.jl
@@ -1,88 +1,8 @@
+include("pdre_api_helpers.jl")
 include("./fixtures/pdre_api_dataset.jl")
 include("./fixtures/pdre_api_results.jl")
 
 using Test
-
-function merge_params(params)
-    #=
-    some = [
-    [[1, 2, 3], [7, 8, 9]],
-    [[4, 5, 6], [10, 11, 12]]
-    ]
-    =#
-    parts = size(params)[1] # 2
-    comps = size(params[1])[1] # 3
-
-    final = []
-    for comp in 1:comps
-        inner = []
-        for part in 1:parts
-        x = params[part][comp]
-        inner = vcat(inner, x)
-        end
-        push!(final, inner)
-    end
-
-    final
-end
-
-function run_dataset(func_list, data_list, cli_list, result_list)
-    # Basic parameters for testing CLIs
-    sub_cmd = "pdre-api"
-    func_arg = "--function"
-    input_arg = "--input"
-
-
-    counter = 1
-    for func in func_list
-        for data in merge_params(data_list)
-            input = ""
-
-            if data == ""
-                # Skip
-            elseif size(data)[1] == 1
-                input = data[1]
-            else
-                set_first = true
-                for entry in data
-                    if set_first
-                        input = data[1]
-                        set_first = false
-                        continue
-                    end
-                    input = join([input, entry], ",")
-                end
-            end
-
-            for cli in cli_list
-                # create first part of the command
-                cmdparams = [cli, sub_cmd, func_arg, func, input_arg]
-                cmd = join(cmdparams, " ")
-
-                # append input
-                cmd = string(cmd, " \"", input, "\"")
-
-                if print_verbose
-                    println("Running: ", cmd)
-                end
-
-                # Run command
-                output = replace(read(`sh -c $cmd`, String), "\n" => "") # remove newline
-                println(output)
-                if result_list != false
-                    @test output == result_list[counter]
-                else
-                    @test true
-                end
-
-                if output != "" && print_verbose
-                    println("> Result: ", output)
-                end
-            end
-            counter = counter + 1
-        end
-    end
-end
 
 @testset "PDRE API Tests **LEGACY**" begin
     script_dir = @__DIR__
--- runtests.jl
+++ runtests.jl
@@ -7,50 +7,11 @@ if length(ARGS) >= 1
     end
 end
 
-function merge_params(params)
-    #=
-    Input = [
-      [[1, 2, 3], [7, 8, 9]],
-      [[4, 5, 6], [10, 11, 12]]
-    ]
-
-    Output = [
-      [1, 2, 3, 4, 5, 6],
-      [7, 8, 9, 10, 11, 12]
-    ]
-    =#
-    parts = size(params)[1] # 2
-    comps = size(params[1])[1] # 3
-
-    final = []
-    for comp in 1:comps
-        inner = []
-        for part in 1:parts
-          x = params[part][comp]
-          inner = vcat(inner, x)
-        end
-        push!(final, inner)
-    end
-
-    final
-end
-
-#=
-Input = [
-  [[1, 2, 3], [7, 8, 9]],
-  [[4, 5, 6], [10, 11, 12]]
-]
-Input = [
-  [[1,2,3],[4,5,6]]
-]
-println(merge_params(Input))
-=#
-
 @time begin
   # @time @testset "Scale Codec Tests" begin include("scale_codec_tests.jl") end
   # @time @testset "State Trie Tests" begin include("state_trie_tests.jl") end
   @time @testset "PDRE API Tests" begin include("pdre_api_tests_legacy.jl") end
-  #@time @testset "PDRE API Tests" begin include("pdre_api_tests.jl") end
+  @time @testset "PDRE API Tests" begin include("pdre_api_tests.jl") end
 end
 
 exit()
