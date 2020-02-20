4550528d0ff2839917100b68f367823aad923a0f
lamafab <42901763+lamafab@users.noreply.github.com>
2020-02-20 17:54:31 +0100

    test for ext_allocator_malloc_version_1

---
+++ test/testers/rust-tester/rust_tester/src/pdre_api/allocator.rs
@@ -0,0 +1,16 @@
+use crate::pdre_api::utils::{Decoder, ParsedInput, Runtime, str};
+use parity_scale_codec::Encode;
+
+pub fn ext_allocator_malloc_version_1(input: ParsedInput) {
+    let mut rtm = Runtime::new();
+
+    let value = input.get(0);
+
+    // Get invalid key
+    let res = rtm
+        .call("rtm_allocator_malloc_version_1", &(value.len() as u32).encode())
+        .decode_val();
+    assert_eq!(res, value);
+
+    println!("{}", str(&res));
+}
