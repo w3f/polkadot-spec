cd1371645113dbf47f54421b84569a90575f0c52
Florian Franzen <Florian.Franzen@gmail.com>
2020-04-28 00:47:33 +0200

    nix: Update w3fpkgs (i.e. kagome)

--- test/nix/w3fpkgs.nix
+++ test/nix/w3fpkgs.nix
@@ -1,8 +1,8 @@
 let
   w3fpkgs = builtins.fetchTarball {
-    name = "w3fpks-2020-04-15";
-    url = https://gitlab.w3f.tech/florian/w3fpkgs/-/archive/c4fffd253eb3804847b0fe7f36facaf6bb2401cf.tar.gz;
-    sha256 = "1slzzn8rw1n26d7nc7ypnk1d4vfjrflk0yiff14nxwwx9a91qadw";
+    name = "w3fpks-2020-04-28";
+    url = https://gitlab.w3f.tech/florian/w3fpkgs/-/archive/768c313dcbcd602c10c734f7890417cf6dda46b3.tar.gz;
+    sha256 = "1zmvwdpzjn547x5cn58p45z8dm4j9s30f3k829x2ivcs6gghdyxm";
   };
 in
   import "${w3fpkgs}/release.nix"
