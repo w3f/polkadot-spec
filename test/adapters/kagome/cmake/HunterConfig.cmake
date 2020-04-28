498e33445d0e50d3efda611dfb8aa02735f00504
Florian Franzen <Florian.Franzen@gmail.com>
2020-04-28 12:20:43 +0200

    ci: clean up kagome build

--- test/adapters/kagome/cmake/HunterConfig.cmake
+++ test/adapters/kagome/cmake/HunterConfig.cmake
@@ -1,4 +1,5 @@
+# TODO: Move away from custom kagome branch for testing
 hunter_config(kagome
-  URL https://github.com/FlorianFranzen/kagome/archive/c77fabe89f49fe159d9bdb3d6787aca30490fc59.zip
-  SHA1 353619cb507ad7a823dd9f71ef191f0e267df463
+  URL https://github.com/FlorianFranzen/kagome/archive/b49b90daef5fff4e1a97a126102510f2eca46759.zip
+  SHA1 e008d0f6d99b727c6cb871f56465562691068039
 )
