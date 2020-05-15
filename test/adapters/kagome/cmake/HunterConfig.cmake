0b2338bc22ed4a66771f699be81f3a97b957f1d7
Yuriy Zarudniy <zarudniy@soramitsu.co.jp>
2020-05-15 19:24:22 +0300

    update kagome version
    Signed-off-by: Yuriy Zarudniy <zarudniy@soramitsu.co.jp>

--- test/adapters/kagome/cmake/HunterConfig.cmake
+++ test/adapters/kagome/cmake/HunterConfig.cmake
@@ -1,5 +1,5 @@
 # TODO: Move away from custom kagome branch for testing
 hunter_config(kagome
-    URL https://github.com/soramitsu/kagome/archive/189edb286d192d755514e7ad88679fd9be31dde7.tar.gz
-    SHA1 4dfb38c607381bb58a5f2fb2e667679ad725e6f4
+    URL https://github.com/soramitsu/kagome/archive/af5c718a05a3b734291dd2c4f574c8dffb76ad46.tar.gz
+    SHA1 81f456bb92d06e7b5861dacb66d4ff77515dadf6
 )
