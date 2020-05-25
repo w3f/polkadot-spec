360789cb528834a77455e3d80775e8c149f22574
Yuriy Zarudniy <zarudniy@soramitsu.co.jp>
2020-05-26 00:42:56 +0300

    fix license headers
    Signed-off-by: Yuriy Zarudniy <zarudniy@soramitsu.co.jp>

--- test/adapters/kagome/cmake/HunterConfig.cmake
+++ test/adapters/kagome/cmake/HunterConfig.cmake
@@ -1,3 +1,20 @@
+#Copyright (c) 2019 Web3 Technologies Foundation
+#
+#This file is part of Polkadot Host Test Suite
+#
+#Polkadot Host Test Suite is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#Polkadot Host Tests is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+
 # TODO: Move away from custom kagome branch for testing
 hunter_config(kagome
     URL https://github.com/soramitsu/kagome/archive/af5c718a05a3b734291dd2c4f574c8dffb76ad46.tar.gz
