66a78db81b221e3d472e64f3fdb27b905a8a29b4
Florian Franzen <Florian.Franzen@gmail.com>
2020-06-15 23:50:37 +0200

    gossamer-adapter: Add host-api skeleton

--- test/adapters/gossamer/adapter.go
+++ test/adapters/gossamer/adapter.go
@@ -20,38 +20,31 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"os"
 )
 
+func usage() {
+	fmt.Println("usage: ", os.Args[0], " <scale-codec|state-trie|host-api> <subcommand-args>")
+}
+
 func main() {
-
-	// Subcommands
-	codecCommand := flag.NewFlagSet("scale-codec", flag.ExitOnError)
-	trieCommand := flag.NewFlagSet("state-trie", flag.ExitOnError)
-	trieCommand := flag.NewFlagSet("host-api", flag.ExitOnError)
-
 	// Verify that a subcommand has been provided
-	// os.Arg[0] is the main command
-	// os.Arg[1] will be the subcommand
-	// os.Arg[2] will be the sub subcommand
 	if len(os.Args) < 2 {
-		fmt.Println("a command amongst \"scale-codec\", \"state-trie\" needs to be specified.")
+		usage()
 		os.Exit(1)
 	}
 
-	// Switch on the subcommand
-	// Parse the flags for appropriate FlagSet
-	// os.Args[2:] will be all arguments starting after the subcommand at os.Args[1]
+	// Parse subcommand and call it with all remaining args
 	switch os.Args[1] {
 	case "scale-codec":
-		ProcessScaleCodecCommand(codecCommand, os.Args[2:])
+		ProcessScaleCodecCommand(os.Args[2:])
 	case "state-trie":
-		ProcessStateTrieCommand(trieCommand, os.Args[2:])
+		ProcessStateTrieCommand(os.Args[2:])
+	case "host-api":
+		ProcessHostApiCommand(os.Args[2:])
 	default:
-		flag.PrintDefaults()
+		usage()
 		os.Exit(1)
 	}
-
 }
