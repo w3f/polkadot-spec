49cdf0e4e841714591bc4ea6a19be20b091309a8
Florian Franzen <Florian.Franzen@gmail.com>
2020-09-14 23:41:13 +0200

    host-spec: Ignore autosaves in diffs and exports

--- host-spec/host-spec.scm
+++ host-spec/host-spec.scm
@@ -4,10 +4,10 @@
 ;; This is a custom compare function with the major difference
 ;; being that this also highlights differences in included files.
 (tm-define (compare-versions-expanded old new)
-  (load-buffer old)
+  (load-buffer old :strict)
   (buffer-expand-includes)
   (with t1 (buffer-tree)
-    (load-buffer new)
+    (load-buffer new :strict)
     (buffer-expand-includes)
     (let* ((t2 (buffer-tree))
            (u1 (tree->stree t1))
@@ -18,10 +18,10 @@
       (tree-set (buffer-tree) rt)
       (version-first-difference))))
 
-;; This is a custom convert function that expands any includes
+;; This is a custom convert function that expands all includes
 ;; before conversion.
 (tm-define (convert-expanded input output)
-  (load-buffer input)
+  (load-buffer input :strict)
   (buffer-expand-includes)
   (export-buffer output))
 
