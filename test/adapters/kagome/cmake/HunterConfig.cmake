b27fb8868a2d234cce0cc6d1292dff40cb20e5b6
Harrm <abrehchs@gmail.com>
2020-11-03 12:01:42 +0300

    kagome-adapter: Minor fixes

--- test/adapters/kagome/cmake/HunterConfig.cmake
+++ test/adapters/kagome/cmake/HunterConfig.cmake
@@ -25,7 +25,7 @@
 #  CMAKE_ARGS TESTING=OFF
 #)
 
-# TODO (yuraz): PRE-451 remove it after collecting libsecp256k1 into hunter config
+# remove it after collecting libsecp256k1 into hunter config
 hunter_config(libsecp256k1
     URL https://github.com/soramitsu/soramitsu-libsecp256k1/archive/c7630e1bac638c0f16ee66d4dce7b5c49eecbaa5.zip
     SHA1 179e316b0fe5150f1b05ca70ec2ac1490fe2cb3b
@@ -34,7 +34,7 @@ hunter_config(libsecp256k1
 
 
 hunter_config(kagome
-  URL https://github.com/soramitsu/kagome/archive/778583ac8ad9d55ac8f06f2b63b5d07cf7f2062c.zip
-  SHA1 05f5408a666e5716da085f2d15a1fbf9b49ce661
-  CMAKE_ARGS TESTING=OFF
+    URL https://github.com/soramitsu/kagome/archive/778583ac8ad9d55ac8f06f2b63b5d07cf7f2062c.zip
+    SHA1 05f5408a666e5716da085f2d15a1fbf9b49ce661
+    CMAKE_ARGS TESTING=OFF
 )
