0a3683dcd91f5532c322e1f7273a8a59d3b263c8
Florian Franzen <Florian.Franzen@gmail.com>
2021-01-07 13:37:27 +0100

    kagome-adapter: propagate libp2p build fix

--- test/adapters/kagome/cmake/HunterConfig.cmake
+++ test/adapters/kagome/cmake/HunterConfig.cmake
@@ -21,7 +21,13 @@ hunter_config(kagome
   CMAKE_ARGS TESTING=OFF
 )
 
-# remove it after collecting libsecp256k1 into hunter config (otherwise causes "unexpected empty string")
+# Remove these after they have been collecting into hunter config (otherwise causes build issues)
+hunter_config(libp2p
+  URL https://github.com/soramitsu/cpp-libp2p/archive/517ca6e4a07db50a87767a3a75258ba3c58776b7.zip
+  SHA1 4c191fcb8e9000f663ee665b13c4a55370101470
+  CMAKE_ARGS TESTING=OFF
+)
+
 hunter_config(libsecp256k1
   URL https://github.com/soramitsu/soramitsu-libsecp256k1/archive/c7630e1bac638c0f16ee66d4dce7b5c49eecbaa5.zip
   SHA1 179e316b0fe5150f1b05ca70ec2ac1490fe2cb3b
