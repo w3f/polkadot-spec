ebd11a2cb5a441451de23b7f0abf52ce17039709
Florian Franzen <Florian.Franzen@gmail.com>
2021-01-10 23:09:02 +0100

    gossamer-adapter: implement storage clear prefix test

--- test/adapters/gossamer/host_api/storage.go
+++ test/adapters/gossamer/host_api/storage.go
@@ -18,9 +18,10 @@
 package host_api
 
 import (
+	"bytes"
 	"fmt"
 	"os"
-	"bytes"
+	"strings"
 
 	"github.com/ChainSafe/gossamer/lib/common/optional"
 	"github.com/ChainSafe/gossamer/lib/runtime"
@@ -202,4 +203,64 @@ func test_storage_exists(r runtime.Instance, key string, value string) {
 
 	// Print result
 	fmt.Println("true")
+}
+
+// Test for rtm_ext_storage_clear_prefix_version_1
+func test_storage_clear_prefix(r runtime.Instance, prefix string, key1 string, value1 string, key2 string, value2 string) {
+	// Insert data
+	storage_set(r, []byte(key1), []byte(value1))
+	storage_set(r, []byte(key2), []byte(value2))
+
+	// Clear prefix
+	prefix_enc, err := scale.Encode([]byte(prefix))
+	if err != nil {
+		fmt.Println("Encoding prefix failed: ", err)
+		os.Exit(1)
+	}
+
+	_, err = r.Exec("rtm_ext_storage_clear_prefix_version_1", prefix_enc)
+	if err != nil {
+		fmt.Println("Execution failed: ", err)
+		os.Exit(1)
+	}
+
+	// Check if first key was handled correctly
+	result1 := storage_get(r, []byte(key1))
+
+	if strings.HasPrefix(key1, prefix) {
+		if result1.Exists() {
+			fmt.Println("Key1 was not deleted")
+			os.Exit(1)
+		}
+	} else {
+		if !result1.Exists() {
+			fmt.Println("Key1 was deleted")
+			os.Exit(1)
+		}
+
+		if !bytes.Equal(result1.Value(), []byte(value1)) {
+			fmt.Printf("Value1 is different: %s\n", result1.Value())
+			os.Exit(1)
+		}
+	}
+
+	// Check if second key was handled correctly
+	result2 := storage_get(r, []byte(key2))
+
+	if strings.HasPrefix(key2, prefix) {
+		if result2.Exists() {
+			fmt.Println("Key2 was not deleted")
+			os.Exit(1)
+		}
+	} else {
+		if !result2.Exists() {
+			fmt.Println("Key2 was deleted")
+			os.Exit(1)
+		}
+
+		if !bytes.Equal(result2.Value(), []byte(value2)) {
+			fmt.Printf("Value2 is different: %s\n", result2.Value())
+			os.Exit(1)
+		}
+	}
 }
