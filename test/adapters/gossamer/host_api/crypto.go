c0355320583bc53eaff9efd6ccb682b00e370ec4
Florian Franzen <Florian.Franzen@gmail.com>
2021-02-24 15:57:31 +0100

    gossamer-adapter: add crypto public keys tests

--- test/adapters/gossamer/host_api/crypto.go
+++ test/adapters/gossamer/host_api/crypto.go
@@ -19,6 +19,7 @@ package host_api
 
 import (
 	"fmt"
+	"bytes"
 
 	"github.com/ChainSafe/gossamer/lib/common/optional"
 	"github.com/ChainSafe/gossamer/lib/runtime"
@@ -51,6 +52,23 @@ func crypto_generate(r runtime.Instance, suite string, seed string) ([]byte, err
 	return pk, nil
 }
 
+// Helper function to call rtm_ext_crypto_<suite>_public_keys_version_1
+func crypto_public_keys(r runtime.Instance, suite string) ([]byte, error) {
+	// Encode input
+	id_enc, err := scale.Encode(DUMY_KEY_ID)
+	if err != nil {
+		return nil, AdapterError{"Encoding key id failed", err}
+	}
+
+	// Request all know public keys
+	keys, err := r.Exec("rtm_ext_crypto_" + suite + "_public_keys_version_1", id_enc)
+	if err != nil {
+		return nil, AdapterError{"Execution failed", err}
+	}
+
+	return keys, nil
+}
+
 // -- Tests --
 
 // Test for ext_crypto_<suite>_generate_version_1
@@ -66,3 +84,43 @@ func test_crypto_generate(r runtime.Instance, suite string, seed string) error {
 	return nil
 }
 
+// Test for ext_crypto_<suite>_public_keys_version_1
+func test_crypto_public_keys(r runtime.Instance, suite string, seed1 string, seed2 string) error {
+	// Generate two new keys
+	pk1, err := crypto_generate(r, suite, seed1)
+	if err != nil {
+		return err
+	}
+
+	pk2, err := crypto_generate(r, suite, seed2)
+	if err != nil {
+		return err
+	}
+
+	// Retrieve all public keys
+	keys, err := crypto_public_keys(r, suite)
+	if err != nil {
+		return err
+	}
+
+	// Check result
+	if len(keys) != 65 || keys[0] != 8 {
+		return newTestFailure("Pubkeys size missmatch")
+	}
+
+	key1 := keys[1:33]
+	key2 := keys[33:65]
+
+	if !bytes.Equal(pk1, key1) && !bytes.Equal(pk1, key2) {
+		return newTestFailure("Keystore does not include pubkey 1")
+	}
+
+	if !bytes.Equal(pk2, key1) && !bytes.Equal(pk2, key2) {
+		return newTestFailure("Keystore does not include pubkey 2")
+	}
+
+	fmt.Printf("1. Public key: %x\n", key1)
+	fmt.Printf("2. Public key: %x\n", key2)
+
+	return nil
+}
