8eea4e98d8c43d0d4e322b3165c1bf138f162b83
Matteo Casonato <matteocasonato@protonmail.com>
2023-03-21 17:45:17 +0100

    Init plugin

--- plugins/graphviz-svg-fixer/index.js
+++ plugins/graphviz-svg-fixer/index.js
@@ -1,14 +1,29 @@
+const fs = require('fs');
+const cheerio = require('cheerio');
+
 module.exports = function (context, options) {
   return {
     name: 'graphviz-svg-fixer',
-	async loadContent() {
-		console.log('loadContent');
+	async postBuild(props) {
+		console.log(props.routesPaths)
+		console.log(props.outDir)
+		const blacklist = [
+			'/404.html',
+			'/',
+		];
+		const routes = props.routesPaths.filter(
+			(route) => !blacklist.includes(route) && !route.startsWith('/__docusaurus')
+		);
+		let htmlFilesToFix = [];
+		for (let route of routes) {
+			let filePath = `${props.outDir}${route}/index.html`;
+			let html = fs.readFileSync(filePath, 'utf8');
+			if (html.includes('graphviz')) {
+				// push to htmlFilesToFix an object  with the route without the / and the html
+				htmlFilesToFix.push({ route: route.slice(1), html });
+			}
+		}
+		// now find all the svg files and fix them
 	},
-    async postBuild({siteConfig = {}, routesPaths = [], outDir}) {
-		// Print out to console all the rendered routes.
-		routesPaths.map((route) => {
-		  console.log(route);
-		});
-	  },
   };
 };
