52fa0dd7278fd38ff5554827b35faae9b6e5e9d1
Noc2 <davidhawig@gmail.com>
2023-04-24 12:55:54 +0200

    Finish first homepage draft

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -10,32 +10,30 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: 'Rust',
+    Svg: require('@site/static/img/rust-svgrepo-com.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        See <code><a href="https://github.com/paritytech/polkadot">Node Implementation by Parity</a></code> and  
+        <code><a href="https://github.com/smol-dot/smoldot">smoldot</a></code>
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: 'Go',
+    Svg: require('@site/static/img/go-svgrepo-com.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        See <code><a href="https://github.com/ChainSafe/gossamer">Gossamer by Chainsafe</a></code>
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: 'C++',
+    Svg: require('@site/static/img/plus_logo_c_icon_214621.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        See <code><a href="https://github.com/soramitsu/kagome">Kagome by Soramitsu</a></code>
       </>
     ),
   },
@@ -45,7 +43,9 @@ function Feature({title, Svg, description}: FeatureItem) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
+      <div className='fillWhite'>
         <Svg className={styles.featureSvg} role="img" />
+        </div>
       </div>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
