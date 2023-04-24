f48814e02ecb323cd02a2bb5cc710a9762b47796
Noc2 <davidhawig@gmail.com>
2023-04-24 11:10:45 +0200

    typing animation landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
+import Typed from 'react-typed';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import styles from './index.module.css';
@@ -12,14 +13,20 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <h1 className="hero__title">
+        Polkadot <Typed
+                strings={[
+                  "Protocol Specification",]}
+                    typeSpeed={120} >
+          </Typed>
+        </h1>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
             to="/polkadot-spec">
-            Read the Spec 🥓
+            Read the Spec
           </Link>
+          
         </div>
       </div>
     </header>
@@ -30,8 +37,8 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description={`${siteConfig.tagline}`}>
       <HomepageHeader />
       <main>
         <HomepageFeatures />
