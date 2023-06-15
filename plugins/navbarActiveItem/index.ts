bf0929a25f57e9e4e8a65ed61629ffbe54f6bb90
Matteo Casonato <matteocasonato@protonmail.com>
2023-06-15 10:41:55 +0200

    minor fix navbar plugin

--- plugins/navbarActiveItem/index.ts
+++ plugins/navbarActiveItem/index.ts
@@ -3,21 +3,23 @@ import safePluginExecution from "../safePluginExecution";
 const navbarActiveItem = () => {
   const script = () => {
     const breadcrumbs = document.getElementsByClassName('breadcrumbs')[0];
-    const chapter = breadcrumbs.children[1].children[0];
-    const chapterSpan = chapter.tagName === 'A' ? chapter.children[0] : chapter;
-    const chapterName = chapterSpan.innerHTML;
-    const navbar = document.getElementsByClassName('navbar__items--right')[0];
-    const links = navbar.querySelectorAll('a[aria-current="page"]');
-    const activeLinkClass = 'navbar__link--active';
-    links.forEach(link => {
-      if (link.innerHTML != chapterName) {
-        link.classList.remove(activeLinkClass);
-      } else {
-        if (!link.classList.contains(activeLinkClass)) {
-          link.classList.add(activeLinkClass);
+    if (breadcrumbs) {
+      const chapter = breadcrumbs.children[1].children[0];
+      const chapterSpan = chapter.tagName === 'A' ? chapter.children[0] : chapter;
+      const chapterName = chapterSpan.innerHTML;
+      const navbar = document.getElementsByClassName('navbar__items--right')[0];
+      const links = navbar.querySelectorAll('a[aria-current="page"]');
+      const activeLinkClass = 'navbar__link--active';
+      links.forEach(link => {
+        if (link.innerHTML != chapterName) {
+          link.classList.remove(activeLinkClass);
+        } else {
+          if (!link.classList.contains(activeLinkClass)) {
+            link.classList.add(activeLinkClass);
+          }
         }
-      }
-    });
+      });
+    }
   };
 
   return {
